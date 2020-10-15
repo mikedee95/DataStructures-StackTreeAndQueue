6196695f64d845dc72e5a1c303579e7bf889a729
mikedee95 <70868084+mikedee95@users.noreply.github.com>
2020-10-15 14:37:11 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
 <?php
-include_once 'Stack.php';
+//include_once 'Stack.php';
+//
+//$stack = new Stack(5);
+//$stack->push(1);
+//$stack->push(2);
+//$stack->push(3);
+//$stack->pop();
+//print_r($stack);
+//
+include_once 'Queue.php';
 
-$stack = new Stack(5);
-$stack->push(1);
-$stack->push(2);
-$stack->push(3);
-$stack->pop();
-print_r($stack);
-
+$queue = new Queue();
+$queue->enQueue(1);
+$queue->enQueue(2);
+$queue->enQueue(3);
+$queue->enQueue(4);
+$queue->deQueue();
+$queue->deQueue();
+$queue->deQueue();
+$queue->deQueue();
+var_dump($queue->isEmpty());
+//$queue->deQueue();
